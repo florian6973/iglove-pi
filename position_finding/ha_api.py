08223aa514362503cbabc04ecf45b1b830331016
florian6973 <70778912+florian6973@users.noreply.github.com>
2022-01-28 13:04:22 +0100

    Create ha_api.py

---
+++ position_finding/ha_api.py
@@ -0,0 +1,45 @@
+# 172.16.16.22
+# eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIyOTZkZjA5ODNjYjU0MTQxOWY3ZDdlMTgyMTdhMTEyMSIsImlhdCI6MTY0MjU5NjYxMCwiZXhwIjoxOTU3OTU2NjEwfQ.FqvXpK27-NL7qkrlrvBWmVCgvjw41acrdIGihQLlQDI
+
+from requests import get, post
+
+class HomeAssistantAPI:
+  def __init__(self, ip, token):
+    self.url = "http://" + ip + ":8123/api/services" # token local network
+    self.headers = {
+      "Authorization": "Bearer " + token,
+      "content-type": "application/json",
+    }
+    
+  def __prep_data_json__(self, type, name):
+    return json.dumps(
+      {
+        "entity_id": type + "." + name
+      }
+    )
+ 
+  # ex : antela_9w_rgb_cct
+  def light_on(self, name):
+    response = post(self.url + "/light/turn_on", headers=self.headers, data=self.__prep_data_json__("light", name))
+    return response
+  
+  def light_off(self, name):
+    response = post(self.url + "/light/turn_on", headers=self.headers, data=self.__prep_data_json__("light", name))
+    return response
+  
+  # ex : iled_blink
+  def run_script(self, name):
+    response = post(self.url + "/script/turn_on", headers=self.headers, data=self.__prep_data_json__("script", name))
+    return response    
+  
+  # MQTT needs to be configured in Home Assistant before
+  # ex : testTopic
+  def mqtt_top(self, payload, topic="default"):    
+    data = json.dumps({
+        "payload": payload,
+        "topic": topic,
+        "retain": False
+    })
+    response = post(self.url + "/mqtt/publish", headers=self.headers, data=data)
+    return response
+    
